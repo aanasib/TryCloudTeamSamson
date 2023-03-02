582e6029808bdaa840d3285257ba0668c8a8c193
Attia_Nasib <aanasib@cydeo.com>
2023-03-02 12:42:15 -0500

    Created Framework

--- src/Test/java/com/tryCloud/step_definitions/LoginStepDefs.java
+++ src/Test/java/com/tryCloud/step_definitions/LoginStepDefs.java
@@ -17,15 +17,12 @@ public class LoginStepDefs {
         String username =null;
         String password =null;
 
-        if(userType.equalsIgnoreCase("driver")){
-            username = ConfigurationReader.getProperty("driver_username");
-            password = ConfigurationReader.getProperty("driver_password");
-        }else if(userType.equalsIgnoreCase("sales manager")){
-            username = ConfigurationReader.getProperty("sales_manager_username");
-            password = ConfigurationReader.getProperty("sales_manager_password");
-        }else if(userType.equalsIgnoreCase("store manager")){
-            username = ConfigurationReader.getProperty("store_manager_username");
-            password = ConfigurationReader.getProperty("store_manager_password");
+        if(userType.equalsIgnoreCase("user")){
+            username = ConfigurationReader.getProperty("user_username");
+            password = ConfigurationReader.getProperty("user_password");
+        }else if(userType.equalsIgnoreCase("employee")){
+            username = ConfigurationReader.getProperty("employee_username");
+            password = ConfigurationReader.getProperty("employee_password");
         }
         //send username and password and login
         new LoginPage().login(username,password);
