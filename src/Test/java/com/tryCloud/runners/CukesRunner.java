bffceee7f3227fa1b6ebee68a20e903f58507175
MCav <marycavallo@protonmail.com>
2023-03-09 11:06:07 -0500

    rev 11;03 am 3/9/23

--- src/Test/java/com/tryCloud/runners/CukesRunner.java
+++ src/Test/java/com/tryCloud/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/tryCloud/step_definitions",
-        dryRun = true,
-        tags = " @wip",
+        dryRun = false,
+        tags = "",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
