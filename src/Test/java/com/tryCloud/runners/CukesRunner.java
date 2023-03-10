f2187a375a8262c4ab408a9300879a9bc5cb337e
Attia_Nasib <aanasib@cydeo.com>
2023-03-10 08:43:06 -0500

    changes

--- src/Test/java/com/tryCloud/runners/CukesRunner.java
+++ src/Test/java/com/tryCloud/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/tryCloud/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "",
         publish = true //generating a report with public link
 )
