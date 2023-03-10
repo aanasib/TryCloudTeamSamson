2ecf884b8fddbf79ebff260f1c513f0805d72082
Attia_Nasib <aanasib@cydeo.com>
2023-03-10 08:38:46 -0500

    changes

--- src/Test/java/com/tryCloud/step_definitions/US73_UpdateSettingsStepDefs.java
+++ src/Test/java/com/tryCloud/step_definitions/US73_UpdateSettingsStepDefs.java
@@ -4,6 +4,7 @@ import com.tryCloud.pages.UpdateSettingsPage;
 import com.tryCloud.utilities.BrowserUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class US73_UpdateSettingsStepDefs {
      UpdateSettingsPage updateSettingsPage = new UpdateSettingsPage();
@@ -13,10 +14,9 @@ public class US73_UpdateSettingsStepDefs {
         String expectedTitle = "Dashboard - Trycloud";
         BrowserUtils.verifyTitle(expectedTitle);
     }
-    @Then("the user clicks the {string} module")
-    public void the_user_clicks_the_module(String string) {
+    @When("the user clicks the {string} module on dashboard")
+    public void theUserClicksTheModuleOnDashboard(String arg0) {
         updateSettingsPage.files.click();
-
     }
     @Then("user clicks Settings on the left bottom corner")
     public void user_clicks_settings_on_the_left_bottom_corner() {
--- src/Test/resources/features/US73_UdateSettings.feature
+++ src/Test/resources/features/US73_UdateSettings.feature
@@ -6,6 +6,6 @@ Feature: As a user, I should be able to update settings.
    # Given the user is on the login page with required <"role">
 
 Scenario: Verify the users can check any checkbox on the Files page.
-When the user clicks the "Files" module
+  When the user clicks the "Files" module on dashboard
 And user clicks Settings on the left bottom corner
 Then the user should be able to click any checkbox
