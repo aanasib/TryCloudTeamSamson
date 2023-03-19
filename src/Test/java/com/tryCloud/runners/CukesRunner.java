6a0acb1dc3b048402a0046b380dc25f7948baf38
Attia_Nasib <aanasib@cydeo.com>
2023-03-19 19:51:34 -0400

    added smoke and json plugin

--- src/Test/java/com/tryCloud/runners/CukesRunner.java
+++ src/Test/java/com/tryCloud/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/tryCloud/step_definitions",
         dryRun = true,
-        tags = "",
+        tags = "@smoke",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
